1453618b6e5f15b847dd6a3a7c7c164afec41e13
willyserge <willyserge01@gmail.com>
2019-11-08 19:58:47 +0200

    redirection to user profile upon click of a login button

---
+++ UI/js/main.js
@@ -0,0 +1,8 @@
+const login_btn=document.querySelector('#login-btn');
+const home_btn=document.querySelector('#homeToLogin-btn');
+
+login_btn.addEventListener('click',()=>{
+	location.replace("userProfile.html")
+});
+
+
