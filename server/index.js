0cdf97621ef1a64bb137659e9403a29baad56e83
willyserge <willyserge01@gmail.com>
2019-11-18 14:46:16 +0200

    initial commit

--- server/index.js
+++ server/index.js
@@ -3,11 +3,9 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-
 app.get("/", (req, res) => {
-  res.json({ status: "success", message: "Welcome To Broadcaster" });
-});
-
+    res.json({ status: "success", message: "Welcome To Broadcaster" });
+  });
 
 
 const PORT = process.env.PORT || 3000;
