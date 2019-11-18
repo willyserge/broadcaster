e06770d66c77cc9124246b931f389cb636c77ce6
willyserge <willyserge01@gmail.com>
2019-11-18 21:40:34 +0200

    added tests for the signup route

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -8,6 +8,31 @@ const token = process.env.TEST_TOKEN;
 describe("Server", () => {
 
   //testing sign up route
+  describe('POST /api/v1/auth/signup', () => {
+    it('should create a user', (done) => {
+      const user = {
+        firstname: 'user',
+        lastname: 'user ',
+        email: 'user@gmail.com',
+        phoneNumber: '0784079275',
+        username: 'user250',
+        password: 'userPassword',
+      };
+  
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send(user)
+        .end((err, res) => {
+          expect(res.status).to.be.equal(201);
+          expect(res.body.status).to.be.equal(201);
+          expect(res.body).to.be.a('Object');
+          expect(res.body.data).to.have.lengthOf(1);
+          expect(res.body.data[0].token);
+          done();
+          
+        });
+    });
+  });
  
   });
 
