7eeb0f9362b1054cd304895359a6c8e959bb458c
willyserge <willyserge01@gmail.com>
2019-11-20 15:06:33 +0200

    cleaned up unneccessary lines of code

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,7 @@ import User from '../controllers/userController'
 import Records from '../controllers/recordsController'
 import Auth from '../middleware/auth'
 const router=express.Router();
-router.get('/test',(req,res)=>{
-    res.send('in the route');
-})
-//sign up route
+
 router.post('/api/v1/auth/signup',User.signUp);
 router.post('/api/v1/auth/signin',User.signIn);
 router.get('/api/v1/red-flags',Auth.verifyToken,Records.getAllRedFrags);
@@ -14,6 +11,7 @@ router.get('/api/v1/red-flags/:id',Auth.verifyToken,Records.getRedFlagById);
 router.post('/api/v1/red-flags',Auth.verifyToken,Records.createRedFlag);
 router.patch('/api/v1/red-flags/:id/location',Auth.verifyToken,Records.updateLocation);
 router.patch('/api/v1/red-flags/:id/comment',Auth.verifyToken,Records.updateComment);
+
 
 
 
