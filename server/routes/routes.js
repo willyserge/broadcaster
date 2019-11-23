0aa8bc2f83189374f11d3556a20cb47ec15ee452
willyserge <willyserge01@gmail.com>
2019-11-23 16:18:02 +0200

    added admin change status route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -7,13 +7,13 @@ const router=express.Router();
 
 router.post('/api/v1/auth/signup',User.signUp);
 router.post('/api/v1/auth/signin',User.signIn);
-router.get('/api/v1/red-flags',Auth.verifyToken,Admin.authenticate,Records.getAllRedFrags);
+router.get('/api/v1/red-flags',Auth.verifyToken,Records.getAllRedFrags);
 router.get('/api/v1/red-flags/:id',Auth.verifyToken,Records.getRedFlagById);
 router.post('/api/v1/red-flags',Auth.verifyToken,Records.createRedFlag);
 router.patch('/api/v1/red-flags/:id/location',Auth.verifyToken,Records.updateLocation);
 router.patch('/api/v1/red-flags/:id/comment',Auth.verifyToken,Records.updateComment);
 router.delete('/api/v1/red-flags/:id',Auth.verifyToken,Records.deleteRedflag);
-router.patch('/api/v1/red-flags/:id/status',Auth.verifyToken,Records.changeStatus);
+router.patch('/api/v1/red-flags/:id/status',Auth.verifyToken,Admin.authenticate,Records.changeStatus);
 
 
 
