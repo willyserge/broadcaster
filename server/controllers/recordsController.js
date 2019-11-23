c560551269e7d0af7deed0e1b6d4d4f5dc2e46d8
willyserge <willyserge01@gmail.com>
2019-11-23 16:16:42 +0200

    added admin change status route

--- server/controllers/recordsController.js
+++ server/controllers/recordsController.js
@@ -162,6 +162,36 @@ class Records{
 
      }
 
+     static async changeStatus(req,res){
+      const id =parseInt(req.params.id);
+      const redFlag=incidents.find((incident)=>incident.id==id);
+      if(!redFlag){ res.status(404).send({
+        status:404,
+        error: 'a red-flag with the given ID was not found.'
+       })
+      }
+      else{
+         const {error}= await Validate.changeStatus(req.body);
+         if(error) return res.status(400).send({
+           status:400,
+           error:error.details[0].message
+         });
+         redFlag.status = req.body.status;
+         res.status(200).send({
+           status:200,
+           data: [
+             {
+               id:redFlag.id,
+               message: "Changed red-flag record's status"
+             }
+           ]
+           
+       })
+
+      }
+
+     }
+
    }
      
 
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,16 +2,18 @@ import express from 'express';
 import User from '../controllers/userController'
 import Records from '../controllers/recordsController'
 import Auth from '../middleware/auth'
+import Admin from '../middleware/admin'
 const router=express.Router();
 
 router.post('/api/v1/auth/signup',User.signUp);
 router.post('/api/v1/auth/signin',User.signIn);
-router.get('/api/v1/red-flags',Auth.verifyToken,Records.getAllRedFrags);
+router.get('/api/v1/red-flags',Auth.verifyToken,Admin.authenticate,Records.getAllRedFrags);
 router.get('/api/v1/red-flags/:id',Auth.verifyToken,Records.getRedFlagById);
 router.post('/api/v1/red-flags',Auth.verifyToken,Records.createRedFlag);
 router.patch('/api/v1/red-flags/:id/location',Auth.verifyToken,Records.updateLocation);
 router.patch('/api/v1/red-flags/:id/comment',Auth.verifyToken,Records.updateComment);
 router.delete('/api/v1/red-flags/:id',Auth.verifyToken,Records.deleteRedflag);
+router.patch('/api/v1/red-flags/:id/status',Auth.verifyToken,Records.changeStatus);
 
 
 
