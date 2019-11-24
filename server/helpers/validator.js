1c8336149a9075db6f53a401c5b30a79945e92a2
willyserge <willyserge01@gmail.com>
2019-11-24 13:08:38 +0200

    removed unnecessary async on functions

--- server/helpers/validator.js
+++ server/helpers/validator.js
@@ -1,6 +1,6 @@
 import Joi from '@hapi/joi';
 class Validate {
-    static async register(userInfo) {
+    static register(userInfo) {
         const schema = Joi.object({
             firstname: Joi.string().min(3).required(),
             lastname: Joi.string().min(3).required(),
@@ -13,7 +13,7 @@ class Validate {
         return schema.validate(userInfo);
     }
 
-    static async signIn(credentials){
+    static signIn(credentials){
         const schema = Joi.object({
            email: Joi.string().email().required(),
             password: Joi.string().min(6).required()
@@ -22,7 +22,7 @@ class Validate {
         return schema.validate(credentials);
     }
 
-    static async createRedFlag(data){
+    static createRedFlag(data){
         const schema = Joi.object({
             title: Joi.string().min(6).required(),
             comment: Joi.string().min(6).max(150).required(),
@@ -30,19 +30,19 @@ class Validate {
          })
          return schema.validate(data);
     }
-    static async updateLocation(location){
+    static updateLocation(location){
         const schema = Joi.object({
             location: Joi.string().min(6).required(),
          })
          return schema.validate(location);
     }
-    static async updateComment(comment){
+    static updateComment(comment){
         const schema = Joi.object({
             comment: Joi.string().min(10).max(150).required(),
          })
          return schema.validate(comment);
     }
-    static async changeStatus(status){
+    static changeStatus(status){
         const schema = Joi.object({
             status: Joi.string().min(10).required(),
          })
