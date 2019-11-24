30c1ebbd083c51e37b68da97fa81595fa8fb9461
willyserge <willyserge01@gmail.com>
2019-11-24 12:07:25 +0200

    added tests for some endpoints

--- server/test/app.test.js
+++ server/test/app.test.js
@@ -51,7 +51,7 @@ describe("Server", () => {
           expect(res.body).to.be.a('Object');
           expect(res.body.data).to.have.lengthOf(1);
           expect(res.body.data[0].token);
-          done();
+          done(err);
           
         });
     });
@@ -73,6 +73,7 @@ describe("Server", () => {
   //test get red-flag by id
   describe('GET /api/v1/red-flags/id', () => {
     it('should get a red-flag record by its ID', (done) => {
+    
 
       chai.request(app)
         .get('/api/v1/red-flags/1')
@@ -110,12 +111,172 @@ describe("Server", () => {
           expect(res.body).to.be.a('Object');
           expect(res.body.data).to.have.lengthOf(1);
           expect(res.body.data[0].message).to.be.equal("Created red-flag record");;
-          done();
+          done(err);
           
         });
     });
   });
+  //testing the update location route
+  describe('PATCH  /api/v1/red-flags/id/location', () => {
+    it("should successfully update a red-flag's location", (done) => {
+      const newLocation = {
+        location: '14131, 6575',
+      };
+
+      chai.request(app)
+      .patch('/api/v1/red-flags/2/location')
+      .set('x-auth-token',process.env.test_token)
+      .send(newLocation)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body.status).to.be.equal(200);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.data).to.have.lengthOf(1);
+        expect(res.body.data[0].message).to.be.equal("Updated red-flag record's location");
+        
+        done(err);
+      });
+    });
+
+    it('should not find a red-flag record', (done) => {
+
+      chai.request(app)
+      .patch('/api/v1/red-flags/8/location')
+      .set('x-auth-token',process.env.test_token)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res.body.status).to.be.equal(404);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.error).to.be.equal('a red-flag with the given ID was not found.');
+        
+        done(err);
+      });
+    });
+
+    it('should not be able to edit a red-flag location', (done) => {
+
+      chai.request(app)
+      .patch('/api/v1/red-flags/1/location')
+      .set('x-auth-token',process.env.test_token)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(405);
+        expect(res.body.status).to.be.equal(405);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.error).to.be.equal('you can no longer edit the location of this red-flag.');
+        
+        done(err);
+      });
+    });
+  });
+  // testing the update comment endpoint
+  describe('PATCH  /api/v1/red-flags/id/comment', () => {
+    it('should successfully update a red-flag comment', (done) => {
+      const newComment = {
+        comment: 'test comment',
+      };
+
+      chai.request(app)
+      .patch('/api/v1/red-flags/2/comment')
+      .set('x-auth-token',process.env.test_token)
+      .send(newComment)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body.status).to.be.equal(200);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.data).to.have.lengthOf(1);
+        expect(res.body.data[0].message).to.be.equal("Updated red-flag record's comment");
+        
+        done(err);
+      });
+    });
+
+    it('should not find a red-flag record', (done) => {
+
+      chai.request(app)
+      .patch('/api/v1/red-flags/8/comment')
+      .set('x-auth-token',process.env.test_token)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res.body.status).to.be.equal(404);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.error).to.be.equal('a red-flag with the given ID was not found.');
+        
+        done(err);
+      });
+    });
+  });
+  //testing delete red-flag route
+  describe('DELETE  /api/v1/red-flags/id', () => {
+    it('should successfully delete a red-flag record', (done) => {
+  
+      chai.request(app)
+      .delete('/api/v1/red-flags/2')
+      .set('x-auth-token',process.env.test_token)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(200);
+        expect(res.body.status).to.be.equal(200);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.data).to.have.lengthOf(1);
+        expect(res.body.data[0].message).to.be.equal("red-flag record has been deleted");
+        
+        
+        done(err);
+      });
+    });
+
+    it('should not find a red-flag record ', (done) => {
+
+      chai.request(app)
+      .delete('/api/v1/red-flags/8')
+      .set('x-auth-token',process.env.test_token)
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        expect(res.body.status).to.be.equal(404);
+        expect(res.body).to.be.a('Object');
+        expect(res.body.error).to.be.equal('a red-flag with the given ID was not found.');
+        
+        done(err);
+      });
+    });
+  });
+
+  //admin change status route
+
+ describe('PATCH  /api/v1/red-flags/id/status', () => {
+  it('should successfully change the status of a red-flag', (done) => {
+    const newStatus = {
+      status: 'under investigation',
+    };
+
+    chai.request(app)
+    .patch('/api/v1/red-flags/1/status')
+    .set('x-auth-token',process.env.test_token)
+    .send(newStatus)
+    .end((err, res) => {
+      expect(res.status).to.be.equal(200);
+      expect(res.body.status).to.be.equal(200);
+      expect(res.body).to.be.a('Object');
+      expect(res.body.data).to.have.lengthOf(1);
+     expect(res.body.data[0].message).to.be.equal("Changed red-flag record's status");
+      
+      done(err);
+    });
+  });
+
+    it('should not find a red-flag record ', (done) => {
+
+      chai.request(app)
+      .patch('/api/v1/red-flags/8/status')
+      .set('x-auth-token',process.env.test_token)
+      .end((err, res) => {
+        expect(res.body.status).to.be.equal(404);
+        
+        done(err);
+      });
+    });
+  });
  
+
   });
 
 
