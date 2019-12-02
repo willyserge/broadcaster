f15b3ab1d83ba2971f2c46803c312203ca94bfdd
willyserge <willyserge01@gmail.com>
2019-12-02 15:47:13 +0200

    edit the incident model

--- server/db/models/incidents.js
+++ server/db/models/incidents.js
@@ -5,10 +5,10 @@ export default {
     createdBy INT NOT NULL,
     title VARCHAR(20) NOT NULL,
     comment VARCHAR(255) NOT NULL,
-    type VARCHAR(255) DEFAULT 'draft' NOT NULL,
+    type VARCHAR(20) NOT NULL,
     location VARCHAR(255) NOT NULL,
     images VARCHAR(255),
-    status VARCHAR(255) NOT NULL,
+    status VARCHAR(20) DEFAULT 'draft' NOT NULL,
     createdOn TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
             --relationship-- 
    FOREIGN KEY( createdBy ) REFERENCES users( id ) ON DELETE CASCADE
