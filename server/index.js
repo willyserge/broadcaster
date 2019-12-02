36782136f2a1fba6ac6bc4ba456e9b237a4246ec
willyserge <willyserge01@gmail.com>
2019-12-02 15:13:39 +0200

    import database models

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import 'express-async-errors';
 import '@babel/polyfill';
 import express from 'express';
 import dotenv from 'dotenv';
+import './db';
 
 import router from './routes/authRoutes';
 import recordsRouter from './routes/recordsRoutes';
