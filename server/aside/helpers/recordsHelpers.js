bf1fc301478aa6b3594493dca47de6fa77832117
willyserge <willyserge01@gmail.com>
2019-12-06 12:16:36 +0200

    remove unnecessary files

--- server/aside/helpers/recordsHelpers.js
+++
@@ -1,6 +0,0 @@
-const RedFlag = {
-  findById(id, userId, incidents) {
-    return incidents.find((incident) => incident.id === id && incident.createdBy === userId);
-  },
-};
-export default RedFlag;
